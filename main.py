43f260cbe18f4d04a390b0643b0b51db09604791
panus13042536 <panus13042536@gmail.com>
2024-10-31 16:10:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "add import io"}
+    return {"message": "face-recognition"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
