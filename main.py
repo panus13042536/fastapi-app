8e9296e13311e3221a46d6d5abd7ee88b145d29a
panus13042536 <panus13042536@gmail.com>
2024-10-31 15:36:36 +0700

    Update main.py add faces_recognition

--- main.py
+++ main.py
@@ -12,3 +12,7 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/faces_recognition/")
+async def faces_recognition(image_upload: UploadFile = File(...)):
+    return {"faces": "PackTest"}
