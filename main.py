866d2071e16a311c67cb1afbc97140cb6b1f290d
panus13042536 <panus13042536@gmail.com>
2024-10-31 15:39:16 +0700

    Update main.py add import io

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-
+import io
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "add import io"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
